dcaf5a869d9edc55ca8e0c2f3e267675b39ae2ce
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-14 20:46:29 -0300

    Add ResourceNotFoundDetails patternBuilder /exception

---
+++ src/main/java/com/br/restApi/exception/ResourceNotFoundDetails.java
@@ -0,0 +1,56 @@
+package com.br.restApi.exception;
+
+
+public class ResourceNotFoundDetails extends ErrorDetails {
+
+    public static final class Builder {
+        private String title;
+        private int status;
+        private String detail;
+        private long timestamp;
+        private String developerMessage;
+
+        
+        private Builder() {
+        }
+
+        public static Builder newBuilder() {
+            return new Builder();
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder status(int status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder detail(String detail) {
+            this.detail = detail;
+            return this;
+        }
+
+        public Builder timestamp(long timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public Builder developerMessage(String developerMessage) {
+            this.developerMessage = developerMessage;
+            return this;
+        }
+
+        public ResourceNotFoundDetails build() {
+            ResourceNotFoundDetails validationErrorDetails = new ResourceNotFoundDetails();
+           validationErrorDetails.setDeveloperMessage(developerMessage);
+           validationErrorDetails.setTitle(title);
+           validationErrorDetails.setDetail(detail);
+           validationErrorDetails.setTimestamp(timestamp);
+           validationErrorDetails.setStatus(status);
+            return validationErrorDetails;
+        }
+    }
+}
